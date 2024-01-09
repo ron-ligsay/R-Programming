961c1be215149a06c89518895119a35bd268cec0
ron-ligsay <akyligsay16@gmail.com>
2024-01-09 18:00:30 +0800

    metric

--- basics.R
+++ basics.R
@@ -1,2 +1,48 @@
 
 # R basics
+print("ako")
+
+demo()
+help()
+
+
+q()
+
+x <- c(1:10)
+x[(x>8)|(x<5)]
+x > 8
+
+
+x <- c(1:15)
+x > 8
+x[c(T,T,T,T,F,F,F,F,T,T)]
+
+
+x <- sin(9)/75
+x <- sin(9)/7
+y = log(x) + x^2
+x
+y
+
+x = 5
+x <- 5
+x = 40
+y = 69
+x + 1
+x - y
+x*y
+x/y
+x^y
+x %/% y
+x < y
+x > y
+
+x || y
+
+(x || y && x + y) && (x || y && x - y)
+
+500 -> x
+X <-10.8
+Y <- 10.8
+x == X
+y == Y
