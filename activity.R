a554b67a314a817b5b7648b8f441e86494161538
ron-ligsay <akyligsay16@gmail.com>
2024-01-09 18:00:47 +0800

    r activity

---
+++ activity.R
@@ -0,0 +1,61 @@
+
+num1 <-  10
+num2 <- 5
+
+# Arithmetic
+sum_result = num1 + num2
+diff_result = num1 - num2
+prod_result = num1 * num2
+div_result = num1 / num2
+mod_result = num1 %% num2
+
+print(paste("Sum :", sum_result))
+print(paste("Difference :",diff_result))
+print(paste("Product :",prod_result))
+print(paste("Division :",div_result))
+print(paste("Mod :", mod_result))
+
+
+#2
+num1 <- 37
+num2 <- 19
+
+# relational operations
+less_result = num1 < num2
+greater_result = num1 > num2
+less_equal_result = num1 <= num2
+greater_equal_result = num1 >= num2
+equal_result = num1 == num2
+
+print(paste("Less than:", less_result))
+print(paste("Greater than:",greater_result))
+print(paste("Less than or equal to:",less_equal_result))
+print(paste("Greater than or equal to:",greater_equal_result))
+print(paste("Equal to:", equal_result))
+
+
+#3
+num1 <- 50
+num2 <- 70
+
+# logical operations
+logical_not_result = !num1
+elementwise_and_result = num1 & num2
+logical_and_result = num1 && num2
+elementwise_or_result = num1 | num2
+logical_or_result = num1 || num2
+
+print(paste("Logical  NOT:", logical_not_result))
+print(paste("Element-wise logical AND:",elementwise_and_result))
+print(paste("Logical AND:",logical_and_result))
+print(paste("Element-wise logical OR:",elementwise_or_result))
+print(paste("Logical OR:", logical_or_result))
+
+
+
+
+
+
+
+
+
